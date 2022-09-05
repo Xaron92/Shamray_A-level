f4f2806a64d1b670047eff9281047a89df813835
Артур Шамрай <arthur.shamrai92@gmail.com>
2022-09-05 22:23:47 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+public class Main { public static void main(String[] args) throws Exception {
+    Date date = new Date();
+    System.out.println("Artur");
+    System.out.println("Shamrai");
+    System.out.println("Viacheslavovich");
+    System.out.println(date);
+            }
+}
